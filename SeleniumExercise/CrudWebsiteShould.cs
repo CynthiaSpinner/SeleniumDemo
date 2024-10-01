1655bf3e871a74e848796b187b4d4930b4fe7f21
Cynthia <CynthiaSpinner@gmail.com>
2024-10-01 13:45:25 -0500

    Selenium demo practice

---
+++ SeleniumExercise/CrudWebsiteShould.cs
@@ -0,0 +1,26 @@
+﻿using SeleniumExercise.PageObjects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SeleniumExercise
+{
+    public class CrudWebsiteShould
+    {
+        [Test]
+
+        public void GoToHomePage()
+        {
+            //Arrange
+            HomePage homePage = new HomePage();
+
+            //Act
+            string actual = homePage.GoToHomePage();
+
+            //Assert
+            Assert.AreEqual("Home Page - InClassDemo", actual);
+        }
+    }
+}
---
+++ SeleniumExercise/PageObjects/HomePage.cs
@@ -0,0 +1,28 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SeleniumExercise.PageObjects
+{
+    internal class HomePage
+    {
+        private readonly IWebDriver _driver = new ChromeDriver();
+        private const string HomeUrl = "https://localhost:44399/";
+
+        public string GoToHomePage()
+        {
+            //start the browser maximized
+            _driver.Manage().Window.Maximize();
+
+            //navigate to home page
+            _driver.Navigate().GoToUrl(HomeUrl);
+
+            //return the title so we can check if we're on the right page
+            return _driver.Title;
+        }
+    }
+}
